6623126dd6f77a7e16bf6b0d832214885a7a1c4e
korzynov87 <korzynov@yandex.ru>
2025-11-10 19:47:01 +0300

    :green_square: <feat>(*.py) add standart PEP8

--- data/proxys.py
+++ data/proxys.py
@@ -2,67 +2,74 @@ from proxybroker import Broker
 from fp.fp import FreeProxy
 import asyncio
 
-class Proxys():
+
+class Proxys:
     def __init__(self):
-        '''Класс для работы с различными прокси. В файле настроек settings.json ключь
-          "DEFAULT_PROXY" может принимать значение которая сводится к названию библиотеки,
-            через которую получаем прокси. По умолчанию proxybroker2'''
+        """Класс для работы с различными прокси. В файле настроек settings.json ключь
+        "DEFAULT_PROXY" может принимать значение которая сводится к названию библиотеки,
+          через которую получаем прокси. По умолчанию proxybroker2"""
         pass
-    
+
     def get_proxys_list_proxybroker2(self, limit=10, countries=None):
-        '''Функция возврощает список прокси. 
+        """Функция возврощает список прокси.
         limit - количество прокси (по умолчанию 10)
         countries - страна  по умолчанию None, возможны варианты: US,JP,SG,FR,BN,BR,RU)
-        '''
-        print('Формируем список актуальных proxy от proxybroker2 в количестве:',limit)
-        proxys_list=[]    
+        """
+        print("Формируем список актуальных proxy от proxybroker2 в количестве:", limit)
+        proxys_list = []
+
         async def show(proxies):
             while True:
                 proxy = await proxies.get()
-                if proxy is None: break
-                
-                new_proxy=f'{proxy.host}:{proxy.port}'.split()[0]
-                if new_proxy!= '':
+                if proxy is None:
+                    break
+
+                new_proxy = f"{proxy.host}:{proxy.port}".split()[0]
+                if new_proxy != "":
                     proxys_list.append(new_proxy)
-        
+
         proxies = asyncio.Queue()
         broker = Broker(proxies)
         tasks = asyncio.gather(
-            broker.find(types=['HTTP','HTTPS'], limit=limit, countries=countries),
-            show(proxies))
+            broker.find(types=["HTTP", "HTTPS"], limit=limit, countries=countries),
+            show(proxies),
+        )
 
-        loop = asyncio.get_event_loop()   
+        loop = asyncio.get_event_loop()
         try:
             loop.run_until_complete(asyncio.wait_for(tasks, 30))
         except asyncio.TimeoutError:
             print("RETRYING PROXIES ...")
-        #loop = asyncio.get_event_loop()
-        #loop.run_until_complete(tasks) 
-        
-        return proxys_list
-    
 
-    def get_proxys_list_FreeProxy(self, limit=10, rand=True, https=False, anonym=True, timeout=0.4):
-        '''Функция возврощает список прокси. 
+        return proxys_list
+
+    def get_proxys_list_FreeProxy(
+        self, limit=10, rand=True, https=False, anonym=True, timeout=0.4
+    ):
+        """Функция возврощает список прокси.
         limit - количество прокси (по умолчанию 10);
         rand - получение прокси случайным образом, возможны варианты: True, False;
         https - c поддержкой https, возможны варианты: True, False;
         anonym - анонимный, возможны варианты: True, False;
         timeout - время отклика не больше)
-        '''
-        #https://pypi.org/project/free-proxy/
-        print('Формируем список актуальных proxy от FreeProxy в количестве:',limit)
-        proxys_list=[] 
-        counter=0
-        while counter<limit:
+        """
+        # https://pypi.org/project/free-proxy/
+        print("Формируем список актуальных proxy от FreeProxy в количестве:", limit)
+        proxys_list = []
+        counter = 0
+        while counter < limit:
             try:
-                proxy = FreeProxy(rand=rand, https=https, anonym=anonym, timeout=timeout).get()
-                counter+=1
+                proxy = FreeProxy(
+                    rand=rand, https=https, anonym=anonym, timeout=timeout
+                ).get()
+                counter += 1
                 proxys_list.append(proxy)
             except:
-                pass    
+                pass
         return proxys_list
-'''
+
+
+"""
 def give_proxy_list(index):
     
     with open('Settings/proxy', 'r') as file_proxy:
@@ -101,11 +108,11 @@ def brouser(url):
             #print('x')
             driver.close()
             driver.quit()
-'''
+"""
 
 if __name__ == "__main__":
-    print('-'*50,'START','-'*50)
-    #app=Proxys().get_proxys_list_proxybroker2()
-    app=Proxys().get_proxys_list_FreeProxy()
+    print("-" * 50, "START", "-" * 50)
+    # app=Proxys().get_proxys_list_proxybroker2()
+    app = Proxys().get_proxys_list_FreeProxy()
     print(app)
-    print('='*50,'FINISH','='*50)
+    print("=" * 50, "FINISH", "=" * 50)
--- data/settings.py
+++ data/settings.py
@@ -1,18 +1,20 @@
 import json
 
+
 class Settings:
-    def __init__(self,file_settings='data\settings\settings.json'):
-        ''' Класс настроек, используеться для получения настроек'''
+    def __init__(self, file_settings="data\settings\settings.json"):
+        """Класс настроек, используеться для получения настроек"""
         self.SETTINGS = json.load(open(file_settings))
 
     def get(self, key):
-        '''Функция которая возвращает значение настроек'''
+        """Функция которая возвращает значение настроек"""
         return self.SETTINGS[key]
 
+
 if __name__ == "__main__":
-    '''
+    """
     #для проверки
     set=Settings()
     print(set.get(key='TIME_IN_SITE'))
-    '''
+    """
     pass
